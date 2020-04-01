ee0fc19ff07096d80b924bef26b859aae134357d
igor <kebard1988@gmail.com>
2020-04-01 12:34:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12 * 30;
+            Console.WriteLine(x);
+            Thread.Sleep(4000);
+        }
+    }
+}
---
+++ gg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
